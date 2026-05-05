7aa264affa216ca6192bf1cf3cfc23477968e134
Zbethell <Zack@canadiancheer.com>
2026-05-05 15:55:15 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Report from './Report.jsx'
+
+const path = window.location.pathname;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {path === '/report' ? <Report /> : <App />}
   </React.StrictMode>
 )
